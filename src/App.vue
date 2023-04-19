489e8ec061543cf39a7cab0379be940cae2d8fe6
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-04-19 12:30:18 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import {store} from "./store"
+export default{
+  data(){
+    return{
+      store
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>BOOLFIX</h1>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss"
+</style>
