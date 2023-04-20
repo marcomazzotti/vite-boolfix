9adc4a9ecb67fd542e0d9aa83fe35f7d7e383f6e
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-04-20 13:07:06 +0200

    Milestone 4

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,44 +13,45 @@ export default {
 
 <template>
   <div class="ms-container">
-    <h2 class="my-4">FILM</h2>
-    <div class="row row-cols-auto">
+    <h2 class="py-4">FILM</h2>
+    <div class="wrapper d-flex flex-wrap">
       <div class="ms-card" v-for="item in store.filmArray">
-        <ul>
-          <li>
-              <img :src="`${store.urlImg}${item.poster_path}`" alt="">
-              <h5 v-if="!item.poster_path">IMMAGINE NON DISPONIBILE</h5>
-              <p>Titolo: {{ item.title }}</p>
-              <p>Titolo Originale: {{ item.original_title }}</p>
-              <img v-if="item.original_language === 'it'" src="../assets/img/Flag_of_Italy.png" class="flag" alt="">
-              <img v-else-if="item.original_language === 'en'" src="../assets/img/Flag_of_the_United_Kingdom.png" class="flag" alt="">
-              <img v-else-if="item.original_language === 'es'" src="../assets/img/Flag_of_the_United_Kingdom.png" class="flag" alt="">
-              <p v-else> {{ item.original_language }}</p>
-              <p>Voto: {{ item.vote_average }}</p>
-          </li>
-        </ul>
+        <div class="ms-film-img">
+          <img :src="`${store.urlImg}${item.poster_path}`" alt="">
+          <h3 class="text-center" v-if="!item.poster_path">IMMAGINE NON DISPONIBILE</h3>
+        </div>
+        <div class="ms-film-info">
+          <p><span>Titolo: </span>{{ item.title }}</p>
+          <p><span>Titolo Originale: </span>{{ item.original_title }}</p>
+          <img v-if="item.original_language === 'it'" src="../assets/img/Flag_of_Italy.png" class="ms-flag" alt="">
+          <img v-else-if="item.original_language === 'en'" src="../assets/img/Flag_of_the_United_Kingdom.png" class="ms-flag" alt="">
+          <img v-else-if="item.original_language === 'es'" src="../assets/img/Flag_of_the_United_Kingdom.png" class="ms-flag" alt="">
+          <p v-else><span>Lingua: </span>{{ item.original_language }}</p>
+          <p><span>Voto: </span>{{ item.vote_average }}</p>
+          <p><span>Overview: </span>{{ item.overview }}</p>
+        </div>
       </div>
     </div>
   </div>
 
   <div class="ms-container">
-      <h2 class="my-4">TV SERIES</h2>
-      <div class="row row-cols-auto">
+      <h2 class="py-4">TV SERIES</h2>
+      <div class="row row-cols-4">
         <div class="ms-card" v-for="item in store.seriesArray">
-          <ul>
-          <li>
-
+          <div class="ms-film-img">
             <img :src="`${store.urlImg}${item.poster_path}`" alt="">
-            <h5 v-if="!item.poster_path">Siamo spiacenti, l'immagine non è disponibile</h5>
-            <p>Titolo: {{ item.name }}</p>
-            <p>Titolo Originale: {{ item.original_name }}</p>
-            <img v-if="item.original_language === 'it'" src="../assets/img/Flag_of_Italy.png" class="flag" alt="">
-            <img v-else-if="item.original_language === 'en'" src="../assets/img/Flag_of_the_United_Kingdom.png" class="flag" alt="">
-            <img v-else-if="item.original_language === 'es'" src="../assets/img/Flag_of_the_United_Kingdom.png" class="flag" alt="">
-            <p v-else> {{ item.original_language }}</p>
-            <p>Voto: {{ item.vote_average }}</p>
-          </li>
-          </ul>
+            <h3 v-if="!item.poster_path">IMMAGINE NON DISPONIBILE</h3>
+          </div>
+          <div class="ms-film-info">
+            <p><span>Titolo: </span>{{ item.name }}</p>
+            <p><span>Titolo Originale: </span>{{ item.original_name }}</p>
+            <img v-if="item.original_language === 'it'" src="../assets/img/Flag_of_Italy.png" class="ms-flag" alt="">
+            <img v-else-if="item.original_language === 'en'" src="../assets/img/Flag_of_the_United_Kingdom.png" class="ms-flag" alt="">
+            <img v-else-if="item.original_language === 'es'" src="../assets/img/Flag_of_the_United_Kingdom.png" class="ms-flag" alt="">
+            <p v-else><span>Lingua: </span>{{ item.original_language }}</p>
+            <p><span>Voto: </span>{{ item.vote_average }}</p>
+            <p><span>Overview: </span>{{ item.overview }}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -60,16 +61,41 @@ export default {
 .ms-container{
   padding: 0 1.5rem;
   margin: 0 auto;
-  ul{
-    list-style-type: none;
-    padding-left: 0;
-    padding-bottom: 1rem;
-    p{
-      margin: 0;
+  background-color: black;
+  color: white;
+  .ms-card{
+    width: calc(100% / 5 - 50px);
+    margin-bottom: 50px;
+    border: 1px solid grey;
+    padding: 30px;
+    margin: 10px;
+    .ms-film-img img{
+      width: 100%;
     }
-    .flag{
+    &:hover .ms-film-info{
+    display: block;
+    overflow-y: auto;
+    }
+    &:hover .ms-film-img{
+    display: none;
+    }
+    .ms-flag{
+    width: 50px;
+    }
+    .ms-film-info{
+    display: none;
+    overflow-y: auto;
+    max-height: 500px;
+    span{
+      font-weight:bold;
+    }
+    img{
       width: 50px;
     }
+  }
+  }
+  p{
+    margin: 1rem 0;
   }
 }
 
